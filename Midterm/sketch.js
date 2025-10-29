b4dd9fcd7bbfd6a9c561fb3d8ed3f4dedb3e16d2
Tenn456 <78876655+SirWhereIsMyFood@users.noreply.github.com>
2025-10-29 13:34:56 -0400

    upload

--- Midterm/sketch.js
+++ Midterm/sketch.js
@@ -1,33 +1,53 @@
 let x = 400, y = 100, x1 = 0, y1 = 0
 let xV = 4, yV = 2
 let col1, col2;
-let img,img1
+let img, img1, img2,img3,img4
+let imageShown = false
+let imageShown1 = false
 
-function preload(){
- img = loadImage("tuff.png")
- img1 = loadImage("download.jpeg")
-
+let Music 
+let count = 0
+function preload() {
+  img = loadImage("tuff.png")
+  img1 = loadImage("download.jpeg")
+  img2 = loadImage("download (1).jpeg")
+  Music = loadSound("Coolmusic.mp3")
+  img3 = loadImage("handy.png")
+  img4 = loadImage("Cupcake.jpeg")
 }
 function setup() {
+  
+
 
   createCanvas(windowWidth, windowHeight)
+  // createCanvas(1920,1080)
   col1 = color('#851400')
   col2 = color('#00e2f2')
 
   strokeWeight(3)
- imageMode(CENTER)
+
+  
 
   //framerate(12)
-
+Music.play()
 }
 
 function draw() {
-  background(150)
+  //background(150)
+  background(img4);
+  // imageMode(CENTER)
   fill(col1)
   ellipse(x, y, 50)
   fill(col2)
   ellipse(x1, y1, 50)
+  text("combo x" + count,200,75)
+  textSize(25)
 
+ if (mouseIsPressed === true) {
+    cursor(CROSS, 8, 8);
+  } else {
+    cursor(CROSS);
+  }
   if (x > width) {
     x = 0
 
@@ -50,25 +70,61 @@ function draw() {
     print("HI")
     x = random(width)
     y = random(height)
-    col1 =color(random(255), random(255), random(255))
+    col1 = color(random(255), random(255), random(255))
+    imageShown1 = true
+    setTimeout(disappearImage, 500)
+    count++
+
   }
+  image(img2, 80, 82, 175, 175)
+  if (imageShown1 == true) {
+    image(img1, 80, 82, 175, 175)
+  }
+
   distance = dist(mouseX, mouseY, x1, y1)
 
   if (distance < 25 && mouseIsPressed == true) {
     print("HI")
     x1 = random(width)
     y1 = random(height)
-    col2 =color(random(255), random(255), random(255))
-   image(img,40,41,40,42)
+    col2 = color(random(255), random(255), random(255))
+    imageShown = true
+    setTimeout(disappearImage, 500)
+    count++
   }
-  y+= yV
-  x+= xV
+
+  if (imageShown == true) {
+    image(img, 80, 82, 175, 175)
+
+
+  }
+
+  y += yV
+  x += xV
   x1++
   y1++
 
-  //image(img1,40,41,80,82)
-  
+  //image(img1,80,82,175,175)
+
 }
+function disappearImage() {
+  imageShown = false
+  imageShown1 = false
+
+}
+let mySound;
+
+  
 
 
+
+ 
+
+
+  
+
+  
+ 
+
+audioplay()
 
